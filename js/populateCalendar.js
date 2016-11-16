8fdcff6763713e70e9597938b75c90a122a251c5
Gen Fillipow <gfillipo@uci.edu>
2016-11-16 13:36:00 -0800

    Initialize Week View Table

--- js/populateCalendar.js
+++ js/populateCalendar.js
@@ -1,4 +1,5 @@
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var weekAndDayView = ["Time", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var date = new Date();
 var month = date.getMonth();
@@ -21,7 +22,7 @@ function createMonthTable(){
    }
 };
 
-/*function createWeekTable(){
+function createWeekTable(){
    document.querySelector("#calendar").innerHTML = "";
    document.querySelector("#calendar").classList = "";
    document.querySelector("#calendar").classList.add("week");
@@ -30,16 +31,14 @@ function createMonthTable(){
    for(var i=0; i<25; i++){
       var tr = tbl.insertRow(i);
       for(var j=0; j<8; j++){
-         if(i===0 && j===0){
-            tr.insertCell(j).outerHTML = "<th>Time</th>";
-         }else if (i===0) {
-            tr.insertCell(j).outerHTML = "<th>"+daysOfWeek[j]+"</th>";
+         if (i===0) {
+            tr.insertCell(j).outerHTML = "<th>"+weekAndDayView[j]+"</th>";
          }else{
             tr.insertCell(j);
          }
       }
    }
-}*/
+}
 
 function populateMonth(){
    var newDate = new Date(year, month+1, 0);
@@ -59,12 +58,17 @@ function populateMonth(){
    }
 };
 
+//Initial Calendar Creation
+////////////////////////////////////////////////////
 createMonthTable();
 populateMonth();
+////////////////////////////////////////////////////
 
 var next = document.querySelector("#nextBtn");
 var prev = document.querySelector("#prevBtn");
 var monthBtn = document.querySelector("#monthlyView");
+var weekBtn = document.querySelector("#weeklyView");
+
 
 next.addEventListener("click", function(e){
    month = month + 1;
@@ -84,4 +88,11 @@ monthBtn.addEventListener("click", function(e){
    year = date.getFullYear();
    createMonthTable();
    populateMonth();
+});
+
+weekBtn.addEventListener("click", function(e){
+   date = new Date();
+   month = date.getMonth();
+   year = date.getFullYear();
+   createWeekTable();
 });
