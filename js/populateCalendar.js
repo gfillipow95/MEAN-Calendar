0553f96d113c4294b09f2aabc6030dbe7fb6e35e
Gen Fillipow <gfillipo@uci.edu>
2016-11-26 17:03:28 -0800

    POST and GET working

--- js/populateCalendar.js
+++ js/populateCalendar.js
@@ -9,9 +9,10 @@ var month = date.getMonth();
 var year = date.getFullYear();
 var eventList = [];
 var event ={};
+const monthFormat = new Intl.DateTimeFormat('ja-JP-u-ca-japanese', {month: 'long'});
 
-var testDateFormat = new Intl.DateTimeFormat('en-US', {month: '2-digit', weekday: "long", month: 'short'});
-//console.log(testDateFormat.format(date))
+var testDateFormat = new Intl.DateTimeFormat('en-US', {weekday: 'long'});
+console.log(testDateFormat.format(date));
 
 function createMonthTable(){
    document.querySelector("#calendar").innerHTML = "";
@@ -25,6 +26,27 @@ function createMonthTable(){
             tr.insertCell(j).outerHTML = "<th>"+ daysOfWeek[j]+"</th>";
          }else{
             tr.insertCell(j);
+         }
+      }
+   }
+};
+
+function populateMonth(){
+   var newDate = new Date(year, month+1, 0);
+   var days = newDate.getDate();
+   var count = 1;
+   var calendar=document.querySelector(".month");
+   var firstDayMonth = new Date(year, month, 1);
+   var startDate = firstDayMonth.getDay();
+   //var cHead = document.querySelector(".calendarHeader").innerHTML = months[month%12] + " " + firstDayMonth.getFullYear();
+   $(".calendarHeader").text(monthFormat.format(newDate) + " " + firstDayMonth.getFullYear());
+   for(var i = 1; i < 7; i++){
+      for(var j = 0; j < 7; j++){
+         if(count <= days && (j >= startDate || i > 1)){
+            calendar.rows[i].cells[j].outerHTML = "<td data-date=" + (firstDayMonth.getMonth()+1) + "/" + count + "/" +
+                                                      firstDayMonth.getFullYear() + ">" + count + "</td>";
+            calendar.rows[i].cells[j].setAttribute("class", "monthDays");
+            count++;
          }
       }
    }
@@ -45,6 +67,23 @@ function createWeekTable(){
          }
       }
    }
+};
+
+function populateWeek(){
+   var calendar=document.querySelector(".week");
+   date.setDate(date.getDate()-date.getDay());
+   document.querySelector(".calendarHeader").innerHTML=(monthAbv[date.getMonth()]) + " " + (date.getDate()-date.getDay());
+   for(var i=0; i<25; i++){
+      for(var j=0; j<8; j++){
+         if(i===0 && j!==0){
+            calendar.rows[i].cells[j].innerHTML += (date.getMonth()+1) + "/" + (date.getDate());
+            date.setDate(date.getDate() + 1);
+         }else if(i>0 && j===0){
+            calendar.rows[i].cells[j].innerHTML = times[i-1];
+         }
+      }
+   }
+   document.querySelector(".calendarHeader").innerHTML += " - " + monthAbv[date.getMonth()] + " " + (date.getDate()-1);
 };
 
 function createDayTable(){
@@ -81,44 +120,6 @@ function populateDay(){
       }
    }
 }
-
-function populateWeek(){
-   var calendar=document.querySelector(".week");
-   date.setDate(date.getDate()-date.getDay());
-   document.querySelector(".calendarHeader").innerHTML=(monthAbv[date.getMonth()]) + " " + (date.getDate()-date.getDay());
-   for(var i=0; i<25; i++){
-      for(var j=0; j<8; j++){
-         if(i===0 && j!==0){
-            calendar.rows[i].cells[j].innerHTML += (date.getMonth()+1) + "/" + (date.getDate());
-            date.setDate(date.getDate() + 1);
-         }else if(i>0 && j===0){
-            calendar.rows[i].cells[j].innerHTML = times[i-1];
-         }
-      }
-   }
-   document.querySelector(".calendarHeader").innerHTML += " - " + monthAbv[date.getMonth()] + " " + (date.getDate()-1);
-};
-
-
-function populateMonth(){
-   var newDate = new Date(year, month+1, 0);
-   var days = newDate.getDate();
-   var count = 1;
-   var calendar=document.querySelector(".month");
-   var firstDayMonth = new Date(year, month, 1);
-   var startDate = firstDayMonth.getDay();
-   var cHead = document.querySelector(".calendarHeader").innerHTML = months[month%12] + " " + firstDayMonth.getFullYear();
-   for(var i = 1; i < 7; i++){
-      for(var j = 0; j < 7; j++){
-         if(count <= days && (j >= startDate || i > 1)){
-            calendar.rows[i].cells[j].outerHTML = "<td data-date=" + (firstDayMonth.getMonth()+1) + "/" + count + "/" +
-                                                      firstDayMonth.getFullYear() + ">" + count + "</td>";
-            calendar.rows[i].cells[j].setAttribute("class", "monthDays");
-            count++;
-         }
-      }
-   }
-};
 
 function createRightDrawer(dateArray/*11/12/2016 --> ["11", "13", "2016"]*/){
    var rd = document.querySelector("#rightDrawer");
